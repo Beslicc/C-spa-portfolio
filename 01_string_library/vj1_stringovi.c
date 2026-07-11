8f6fa4bd60a312f07bb263fca9552579d7fe5090
Beslicc <beslicjosip0@gmail.com>
2026-07-11 17:15:12 +0200

    Implement mystrcat in VJ1

--- 01_string_library/vj1_stringovi.c
+++ 01_string_library/vj1_stringovi.c
@@ -35,9 +35,24 @@ int mystrcmp(const char* s1, const char* s2) {
     return s1[i] - s2[i];
 }
 
+void mystrcat(char* dest, const char* src){
+    int i = 0;
+    while (dest[i] != '\0') {
+        i++;
+    }
+    int j = 0;
+    while (src[j]!='\0') {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
+}
+
 int main(void) {
 	char text[] = "Hello";
     char kopija[50];
+    char text2[50] = "World";
 
     printf("Zadatak1:\n");
     printf("Length: %d\n", mystrlen(text));
@@ -50,5 +65,9 @@ int main(void) {
     printf("abc vs abc: %d\n", mystrcmp("abc", "abc"));
     printf("abc vs abd: %d\n", mystrcmp("abc", "abd"));
     
+    mystrcat(text2, text);
+    printf("Zadatak4:\n");
+    printf("Spojeni string: %s\n", text2);
+
     return 0;
 }
