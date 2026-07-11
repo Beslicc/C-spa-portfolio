6274db2a77957534264eb8f12753dbebf22daf66
Beslicc <beslicjosip0@gmail.com>
2026-07-11 16:46:24 +0200

    Implement mystrlen and mystrcpy in VJ1

--- 01_string_library/vj1_stringovi.c
+++ 01_string_library/vj1_stringovi.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 
+int mystrlen(char* niz) {
+    int duzina = 0;
+    
+    while (niz[duzina] != '\0') {
+        duzina++;
+    }
+
+    return duzina;
+}
+
+void mystrcpy(char* dest, char* src) {
+    int i = 0;
+    
+    while (src[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
+
+    dest[i] = '\0';
+}
 
 int main(void) {
 	char text[] = "Hello";
+    char kopija[50];
 
-    printf("Text: %s\n", text);
+    printf("Zadatak1:\n");
     printf("Length: %d\n", mystrlen(text));
 
-    printf("Length empty: %d\n", mystrlen(""));
-    printf("Length abc: %d\n", mystrlen("abc"));
-    printf("Length programming: %d\n", mystrlen("programming"));
+    mystrcpy(kopija,text);
+    printf("Zadatak2:\n");
+    printf("Kopirani string: %s\n", kopija);
 
     return 0;
 }
